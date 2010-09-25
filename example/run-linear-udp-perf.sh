81f7a41a3a1d8947e78d2daded84c97bf7cbacb4
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2010-09-25 17:40:57 +0200

    report better names

--- example/run-linear-udp-perf.sh
+++ example/run-linear-udp-perf.sh
@@ -3,12 +3,17 @@ nodes="1 2 3 4 8 10 16 20"
 DCE_RUNNER="./build/optimized/src/dce/utils/dce-runner"
 PRGM="./build/optimized/src/dce/example/linear-udp-perf"
 CONSTANTS="--TxBandwidth=1000000 --LinkBandwidth=2MBps --ns3::TaskManager::FiberManagerType=UcontextFiberManager --Duration=200s"
-args_exp1="--Ns3App=true"
-args_exp2="--Ns3App=false"
-args_exp3="--Ns3App=false --LinuxPath=./libnet-next-2.6.so"
+args_a="--Ns3App=true"
+args_b="--Ns3App=false"
+args_c="--Ns3App=false --LinuxPath=./libnet-next-2.6.so"
+name_a="dce-none"
+name_b="dce-user"
+name_c="dce-user+kernel"
 for node in ${nodes}; do
-    for exp in exp1 exp2 exp3; do
-	echo -n "${node},${exp},";
+    for exp in a b c; do
+	tmp=name_${exp}
+	NAME=${!tmp}
+	echo -n "${node},${NAME},";
         MEM=0;
         for i in 0 1 2 3 4 5 6 7 8 9; do
 	    tmp=args_${exp}
