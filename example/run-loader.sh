6f184ae5f2bc6eb610c0ea80d8a34594708a7d6b
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2010-09-25 17:40:46 +0200

    make sure we get same results of run-linear

--- example/run-loader.sh
+++ example/run-loader.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 nodes="1 2 3 4 8 10 16 20 32 50 63"
 loaders="Cooja Dlm Copy"
-ARGS="--ns3::TaskManager::FiberManagerType=UcontextFiberManager --LinuxPath=./libnet-next-2.6-pic.so"
+ARGS="--TxBandwidth=1000000 --LinkBandwidth=2MBps --ns3::TaskManager::FiberManagerType=UcontextFiberManager --LinuxPath=./libnet-next-2.6-pic.so --Duration=200s"
 PRGM="./build/optimized/src/dce/example/linear-udp-perf"
 DCE_RUNNER="./build/optimized/src/dce/utils/dce-runner"
 for node in ${nodes}; do
