c3fbee41e7cac07c2567421be21a75387e3da7e6
Mathieu Lacage <mathieu.lacage@inria.fr>
2011-01-31 14:18:13 +0100

    simplest example possible

---
+++ example/dce-tcp-simple.cc
@@ -0,0 +1,34 @@
+#include "ns3/helper-module.h"
+#include "ns3/simulator-module.h"
+#include "ns3/core-module.h"
+
+using namespace ns3;
+
+int main (int argc, char *argv[])
+{
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+
+  NodeContainer nodes;
+  nodes.Create (1);
+
+  InternetStackHelper stack;
+  stack.Install (nodes);
+
+  DceManagerHelper dceManager;
+  dceManager.Install (nodes);
+
+  DceApplicationHelper dce;
+  ApplicationContainer apps;
+
+  dce.SetBinary ("tcp-loopback");
+  dce.SetStackSize (1<<20);
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (4.0));
+
+  Simulator::Stop (Seconds(1000100.0));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
