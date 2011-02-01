d99d772abc197cf187e6065f118aceb0a1622c16
Mathieu Lacage <mathieu.lacage@inria.fr>
2011-02-01 13:46:40 +0100

    new simple example for linux kernel

---
+++ example/dce-linux-simple.cc
@@ -0,0 +1,59 @@
+#include "ns3/helper-module.h"
+#include "ns3/simulator-module.h"
+#include "ns3/core-module.h"
+
+using namespace ns3;
+
+int main (int argc, char *argv[])
+{
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+
+  NodeContainer nodes;
+  nodes.Create (1);
+
+  DceManagerHelper dceManager;
+  dceManager.SetNetworkStack("ns3::LinuxSocketFdFactory",
+			     "Library", StringValue ("libnet-next-2.6.so"));
+  dceManager.Install (nodes);
+
+  DceApplicationHelper dce;
+  ApplicationContainer apps;
+  dce.SetBinary ("./ip");
+  dce.SetStackSize (1<<16);
+
+  dce.ResetArguments();
+  dce.ParseArguments("-f inet addr add local 127.0.0.1/8 scope host dev lo");
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (2.0));
+  dce.ResetArguments();
+  dce.ParseArguments("link set lo up");
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (3.0));
+  dce.ResetArguments();
+  dce.ParseArguments("route list table all");
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (3.1));
+
+  dce.ResetArguments();
+  dce.ParseArguments("addr show dev lo");
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (3.0));
+
+  dce.SetBinary ("udp-server");
+  dce.ResetArguments();
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (4.0));
+
+  dce.SetBinary ("udp-client");
+  dce.ResetArguments();
+  dce.AddArgument ("127.0.0.1");
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (4.5));
+
+  Simulator::Stop (Seconds(1000100.0));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
