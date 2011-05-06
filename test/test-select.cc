05d71bc455ac4588364385d1e6f5405eb40e1c71
frederic.urbani@inria.fr <frederic.urbani@inria.fr>
2011-05-06 14:38:31 +0200

    Grow select test in order to debug it

--- test/test-select.cc
+++ test/test-select.cc
@@ -4,10 +4,21 @@
 #include <sys/select.h>
 #include <sys/timerfd.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+
 #include "test-macros.h"
 
 // test, that select () with timeout={0,0} exits immediately
-static void test_select_null_null (void)
+static void
+test_select_null_null (void)
 {
   // create timer to check whether a following call to select() blocks
   int timerfd = timerfd_create (CLOCK_MONOTONIC, 0);
@@ -28,15 +39,427 @@ static void test_select_null_null (void)
   FD_SET (timerfd, &fds);
 
   // doing select() with timeout = {0, 0}
-  struct timeval timeout = {0, 0};
+  struct timeval timeout =
+    { 0, 0 };
   int nfds = select (timerfd + 1, &fds, NULL, NULL, &timeout);
   close (timerfd);
   // no fds must be ready and select() should complete without errors
   TEST_ASSERT_EQUAL (nfds, 0);
 }
 
-int main (int argc, char *argv[])
+static bool
+test_select_read (int fd, int timeOutSec, bool needSuccess)
 {
-  test_select_null_null();
+  fd_set readFd;
+  struct timeval timeOut;
+  int maxFd = fd + 1;
+  int ret = 0;
+
+  timeOut.tv_sec = timeOutSec;
+  timeOut.tv_usec = 0;
+  FD_ZERO (&readFd);
+  FD_SET (fd, &readFd);
+
+  ret = select (maxFd, &readFd, NULL, NULL, &timeOut);
+  if (needSuccess)
+    {
+      return (ret == 1) && (FD_ISSET (fd, &readFd) );
+    }
+  else
+    {
+      return (ret == 0);
+    }
+}
+
+// test, select over stdin
+static void
+test_select_stdin (void)
+{
+  TEST_ASSERT(test_select_read (0, 1, false));
+}
+
+static bool
+wait_write (int fd, int timeOutSec)
+{
+  fd_set writeFd;
+  struct timeval timeOut;
+  int maxFd = fd + 1;
+  int ret = 0;
+
+  timeOut.tv_sec = timeOutSec;
+  timeOut.tv_usec = 0;
+  FD_ZERO (&writeFd);
+  FD_SET ( fd, &writeFd);
+
+  ret = select (maxFd, NULL, &writeFd, NULL, &timeOut);
+  return (1 == ret);
+}
+
+static void
+test_select_write (int fd, int timeOutSec, bool needSuccess)
+{
+  fd_set writeFd;
+  struct timeval timeOut;
+  int maxFd = fd + 1;
+  int ret = 0;
+
+  timeOut.tv_sec = timeOutSec;
+  timeOut.tv_usec = 0;
+  FD_ZERO (&writeFd);
+  FD_SET ( fd, &writeFd);
+
+  ret = select (maxFd, NULL, &writeFd, NULL, &timeOut);
+  if (needSuccess)
+    {
+      TEST_ASSERT_EQUAL ( ret, 1);
+      TEST_ASSERT ( FD_ISSET (fd, &writeFd) );
+    }
+  else
+    {
+      TEST_ASSERT_EQUAL ( ret, 0);
+    }
+}
+
+// test, select over stdout
+static void
+test_select_stdout (void)
+{
+  test_select_write (1, 1, true);
+}
+
+#undef BUF_LEN
+#define BUF_LEN ((size_t) 1024 * 1000 )
+
+static char* readBuf[BUF_LEN];
+static char* writeBuf[BUF_LEN];
+
+// Test, select over TCP Socket.
+static void *
+client1 (void *arg)
+{
+  int sock, sock2;
+  struct sockaddr_in addr;
+  int res;
+
+  sock = socket (AF_INET, SOCK_STREAM, 0);
+  TEST_ASSERT( sock >= 0 );
+  sock2 = socket (AF_INET, SOCK_STREAM, 0);
+  TEST_ASSERT( sock2 >= 0 );
+
+  res = inet_aton ("127.0.0.1", &(addr.sin_addr));
+  TEST_ASSERT_EQUAL ( res, 1);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (1234);
+
+  sleep (1);
+
+  res = connect (sock, (struct sockaddr *) &addr, sizeof(addr));
+  TEST_ASSERT_EQUAL( res, 0 );
+  sleep(1);
+  res = connect (sock2, (struct sockaddr *) &addr, sizeof(addr));
+  TEST_ASSERT_EQUAL( res, 0 );
+
+  // Can I Write ?
+  test_select_write (sock, 1, true);
+  // Read ?
+//  TEST_ASSERT (test_select_read (sock, 1, false)); // XXX : failed with DCE
+  sleep(5);
+  // Read ?
+  TEST_ASSERT (test_select_read (sock, 10, true));
+
+  res = read (sock2, readBuf, BUF_LEN);
+  printf ("Client1: read -> %d \n ",res);
+
+  close (sock);
+  close (sock2);
+
+  printf("Client1: end\n ");
+
+  return arg;
+}
+
+static void *
+server1 (void *arg)
+{
+  int sock, sockin, sockin2;
+  struct sockaddr_in addr;
+  int res;
+
+  sock = socket (AF_INET, SOCK_STREAM, 0);
+  TEST_ASSERT( sock >= 0 );
+
+  res = inet_aton ("127.0.0.1", &(addr.sin_addr));
+  TEST_ASSERT_EQUAL ( res, 1);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (1234);
+
+  res = bind (sock, (struct sockaddr *) &addr, sizeof(addr));
+  printf ("Server1: bind -> %d, errno:%d\n ", res, errno);
+  TEST_ASSERT_EQUAL ( res, 0);
+
+  res = listen (sock, 10);
+  TEST_ASSERT_EQUAL ( res, 0);
+
+  sockin = accept (sock, NULL, NULL);
+  TEST_ASSERT( sockin >= 0 );
+  sleep(1);
+  sockin2 = accept (sock, NULL, NULL);
+  TEST_ASSERT( sockin2 >= 0 );
+
+  sleep (4);
+  res = write (sockin, writeBuf, 1024);
+  sleep (10);
+
+
+  struct timeval tiout;
+  tiout.tv_sec = 2;
+  tiout.tv_usec = 0;
+  res = setsockopt (sockin, SOL_SOCKET, SO_SNDTIMEO, &tiout, sizeof(tiout));
+  TEST_ASSERT_EQUAL (res, 0);
+
+  while ( wait_write(sockin, 10) )
+    { //  Fill the buffer
+      res = write (sockin, writeBuf, BUF_LEN);
+      printf ("Server1: write->%d\n ",res);
+      fflush(stdout);
+    }
+
+  // buffer is full
+  test_select_write (sockin, 0, false);
+
+  res = write (sockin2, writeBuf, 1024);
+
+  close (sock);
+  close (sockin);
+  close (sockin2);
+
+//  sleep(1); // XXX : failed with NS3/DCE do not time out ? the time is stopped ?
+
+  printf ("Server1: end\n ");
+
+  return arg;
+}
+
+#undef BUF_LEN
+#define BUF_LEN ((size_t) 1024 )
+
+// Test, select over UDP Socket.
+static void *
+client2 (void *arg)
+{
+  int sock;
+  struct sockaddr_in addr;
+  int res;
+
+  sock = socket (AF_INET, SOCK_DGRAM, 0);
+  TEST_ASSERT( sock >= 0 );
+
+  res = inet_aton ("127.0.0.1", &(addr.sin_addr));
+  TEST_ASSERT_EQUAL ( res, 1);
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (1234);
+
+  sleep (1);
+
+  res = sendto (sock, writeBuf, BUF_LEN, 0, (const struct sockaddr *) &addr, sizeof(addr));
+
+  TEST_ASSERT_EQUAL ( res, BUF_LEN);
+
+  close (sock);
+
+  printf ("Client2: end\n ");
+
+  return arg;
+}
+
+static void *
+server2 (void *arg)
+{
+  int sock;
+  struct sockaddr_in addr;
+  int res;
+  socklen_t len;
+
+  sock = socket (AF_INET, SOCK_DGRAM, 0);
+  TEST_ASSERT( sock >= 0 );
+
+  res = inet_aton ("127.0.0.1", &(addr.sin_addr));
+  // TEST_ASSERT_EQUAL ( res, 1);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (1234);
+
+  res = bind (sock, (struct sockaddr *) &addr, sizeof(addr));
+  TEST_ASSERT_EQUAL ( res, 0);
+
+  sleep (2);
+
+  // Read ?
+  TEST_ASSERT (test_select_read (sock, 2, true));
+
+  len = sizeof(addr);
+  res = recvfrom (sock, readBuf, BUF_LEN, 0, (struct sockaddr *) &addr, &len);
+  printf ("Server2: recvfrom -> %d\n ", res);
+  TEST_ASSERT_EQUAL ( res, BUF_LEN);
+  sleep (1);
+
+  close(sock);
+
+  printf ("Server2: end\n ");
+
+  return arg;
+}
+
+// Test, select over TCP Socket do select before accept.
+static void *
+client3 (void *arg)
+{
+  int sock;
+  struct sockaddr_in addr;
+  int res;
+
+  sock = socket (AF_INET, SOCK_STREAM, 0);
+  TEST_ASSERT( sock >= 0 );
+
+  res = inet_aton ("127.0.0.1", &(addr.sin_addr));
+  TEST_ASSERT_EQUAL ( res, 1);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (1234);
+
+  sleep (1);
+
+  res = connect (sock, (struct sockaddr *) &addr, sizeof(addr));
+  TEST_ASSERT_EQUAL( res, 0 );
+
+  sleep (1);
+
+  close (sock);
+
+  printf("Client3: end\n ");
+
+  return arg;
+}
+
+static void *
+server3 (void *arg)
+{
+  int sock, sockin;
+  struct sockaddr_in addr;
+  int res;
+
+  sock = socket (AF_INET, SOCK_STREAM, 0);
+  TEST_ASSERT( sock >= 0 );
+
+  res = inet_aton ("127.0.0.1", &(addr.sin_addr));
+  TEST_ASSERT_EQUAL ( res, 1);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (1234);
+
+  res = bind (sock, (struct sockaddr *) &addr, sizeof(addr));
+  printf ("Server3: bind -> %d, errno:%d\n ", res, errno);
+  TEST_ASSERT_EQUAL ( res, 0);
+
+  res = listen (sock, 1);
+  TEST_ASSERT_EQUAL ( res, 0);
+
+  TEST_ASSERT (test_select_read (sock, 10, true));
+
+  sockin = accept (sock, NULL, NULL);
+  TEST_ASSERT( sockin >= 0 );
+  sleep(1);
+
+  close (sock);
+  close (sockin);
+
+  printf ("Server3: end\n ");
+
+  return arg;
+}
+
+static void
+test_select_stdout_stdin (void)
+{
+  fd_set rFd;
+  fd_set wFd;
+  struct timeval timeOut;
+ // int maxFd = 1 + 1;
+  int ret = 0;
+  time_t before, after;
+  int sock = socket (AF_INET, SOCK_DGRAM, 0); // Workaround should be stdin
+
+  timeOut.tv_sec = 10;
+  timeOut.tv_usec = 0;
+  FD_ZERO (&rFd);
+  FD_SET (sock, &rFd);
+  FD_ZERO (&wFd);
+  FD_SET (1, &wFd);
+
+  // should immediately return 1 because stdout is writable.
+  before = time (NULL);
+  ret = select (sock + 1, &rFd, &wFd, NULL, &timeOut);
+  after = time (NULL);
+
+  printf("After select -> %d before time %ld after time %ld errno:%d \n ", ret, before ,after, errno );
+
+  // time difference must not exceed 1 second
+  TEST_ASSERT ( ( 1 == ret ) &&  ( (after - before ) <= 1 ) );
+
+  close(sock);
+
+}
+
+static void
+launch (void *
+(*clientStart) (void *), void *
+(*serverStart) (void *))
+{
+  int status;
+  pthread_t theClient;
+  pthread_t theServer;
+
+  printf ("launch\n ");
+
+  status = pthread_create (&theServer, NULL, serverStart, 0);
+  TEST_ASSERT_EQUAL (status, 0);
+
+  status = pthread_create (&theClient, NULL, clientStart, 0);
+  TEST_ASSERT_EQUAL (status, 0);
+
+  void *threadResult = 0;
+
+  status = pthread_join (theClient, &threadResult);
+  TEST_ASSERT_EQUAL (status, 0);
+
+  status = pthread_join (theServer, &threadResult);
+  TEST_ASSERT_EQUAL (status, 0);
+
+  fflush (stdout);
+  fflush (stderr);
+}
+
+int
+main (int argc, char *argv[])
+{
+  signal (SIGPIPE, SIG_IGN);
+
+  test_select_null_null ();
+  launch (client2, server2);
+  test_select_stdout ();
+  launch (client3, server3);
+
+  if (1 > 2)
+    {
+      test_select_stdin (); // XXX : failed with NS3/DCE
+      launch (client1, server1); // XXX : failed with NS3/DCE
+      test_select_stdout_stdin (); // XXX : failed with NS3/DCE
+    }
+  printf("test-select end.\n ");
+  fflush (stdout);
+  fflush (stderr);
+  sleep(1);
   return 0;
 }
