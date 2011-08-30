e6da5396fc32abd8ebc096a6489270576c5b817b
frederic.urbani@inria.fr <frederic.urbani@inria.fr>
2011-08-30 09:29:49 +0200

    add missing sources !

---
+++ example/ccnx/misc-tools.cc
@@ -0,0 +1,42 @@
+#include "misc-tools.h"
+#include "ns3/constant-position-mobility-model.h"
+//#include "ns3/applications-module.h"
+#include "ns3/dce-module.h"
+
+namespace ns3 {
+
+void setPos (Ptr<Node> n, int x, int y, int z)
+{
+  Ptr<ConstantPositionMobilityModel> loc = CreateObject<ConstantPositionMobilityModel> ();
+  n->AggregateObject (loc);
+  Vector locVec2 ( x, y, z);
+  loc->SetPosition (locVec2);
+}
+
+
+void RunIp (Ptr<Node> node, Time at, std::string str)
+{
+  DceApplicationHelper process;
+  ApplicationContainer apps;
+  process.SetBinary ("./ip");
+  process.SetStackSize (1<<16);
+  process.ResetArguments();
+  process.ParseArguments(str.c_str ());
+  apps = process.Install (node);
+  apps.Start (at);
+}
+
+void AddAddress (Ptr<Node> node, Time at, const char *name, const char *address)
+{
+  std::ostringstream oss;
+  oss << "-f inet addr add " << address << " dev " << name;
+  RunIp (node, at, oss.str ());
+}
+
+std::string Ipv4AddressToString (Ipv4Address ad)
+{
+  std::ostringstream oss;
+  ad.Print (oss);
+  return oss.str ();
+}
+}
---
+++ example/ccnx/misc-tools.h
@@ -0,0 +1,17 @@
+#ifndef MISC_TOOLS_H
+#define MISC_TOOLS_H
+#include "ns3/network-module.h"
+#include "ns3/core-module.h"
+
+namespace ns3 {
+
+  void setPos (Ptr<Node> n, int x, int y, int z);
+
+  void RunIp (Ptr<Node> node, Time at, std::string str);
+
+  void AddAddress (Ptr<Node> node, Time at, const char *name, const char *address);
+
+  std::string Ipv4AddressToString (Ipv4Address ad);
+}
+
+#endif // MISC_TOOLS_H
