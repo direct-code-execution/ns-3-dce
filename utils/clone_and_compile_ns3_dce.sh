4a3173fe73f7a8ec615f11b41320fc4fa61a1ec2
frederic.urbani@inria.fr <frederic.urbani@inria.fr>
2012-01-05 17:44:52 +0100

    Change ns-3-dev used version by this version of DCE.

--- utils/clone_and_compile_ns3_dce.sh
+++ utils/clone_and_compile_ns3_dce.sh
@@ -21,7 +21,7 @@ then
  	hg clone http://code.nsnam.org/furbani/ns-3-linux
 fi	
 echo clone ns-3-dev
-hg clone http://code.nsnam.org/ns-3-dev -r 027aae146ae2
+hg clone http://code.nsnam.org/ns-3-dev -r ab2af491785a
 mkdir build
 cd ns-3-dev
 patch -p1 <../ns-3-dce/utils/packet-socket-upgrade-exp.patch
