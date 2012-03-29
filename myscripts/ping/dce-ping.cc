d407d52300bb1c529a67fe52e398df1ae10c13a1
frederic.urbani@inria.fr <frederic.urbani@inria.fr>
2012-03-29 17:00:46 +0200

    Add dce-emu-ping exemple using emu to do an external ping using a real ping lauched into DCE.

--- myscripts/ping/dce-ping.cc
+++ myscripts/ping/dce-ping.cc
@@ -6,7 +6,7 @@
 #include "ns3/applications-module.h"
 #include "ns3/netanim-module.h"
 #include "ns3/constant-position-mobility-model.h"
-#include "../../example/ccnx/misc-tools.h"
+#include "misc-tools.h"
 
 using namespace ns3;
 
