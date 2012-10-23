d7f505fc6299c6c050b146be4555646bc1012919
frederic.urbani@inria.fr <frederic.urbani@inria.fr>
2012-10-23 17:20:58 +0200

    Add scenario example using HTTP protocol and applications.

---
+++ myscripts/httpd/README
@@ -0,0 +1,50 @@
+This scenario use HTTP protocol using a real server and a real client.
+
+We use 2 externals software to do this experience:
+
+ 1. thttpd which is a light weight HTTP server
+ 
+ 2. wget is a well known HTTP client.
+
+The topology is simple we use 2 nodes linked by a point to point channel.
+
+As you must know to use a real application under NS-3/DCE you need to recompile it. 
+So below there is the recipes to compile the needed softwares for DCE usage.
+
+THTTPD  -------------------------------------------------------------------------------------------------
+
+Below there is the commands you may use to build thttpd :
+
+wget http://www.acme.com/software/thttpd/thttpd-2.25b.tar.gz
+tar xf thttpd-2.25b.tar.gz
+cd thttpd-2.25b
+./configure
+patch -p1 < dce-thttpd.patch
+CFLAGS=-fPIC LDFLAGS=-pie make
+cp thttpd $BASEDCE/build/bin_dce
+
+WGET ----------------------------------------------------------------------------------------------------
+
+Below there is the commands you may use to build wget:
+
+echo to get wget sources you need wget binary :)
+wget http://ftp.gnu.org/gnu/wget/wget-1.14.tar.gz
+tar xf wget-1.14.tar.gz
+cd wget-1.14/
+CFLAGS=-fPIC LDFLAGS=-pie ./configure --disable-opie  --disable-digest --disable-ntlm  --disable-largefile --disable-threads --disable-nls  --disable-rpath  --disable-iri --without-ssl --without-zlib  --without-libiconv-prefix --without-libintl-prefix --without-libpth-prefix  --without-included-regex
+make
+cp src/wget $BASEDCE/build/bin_dce
+
+---------------------------------------------------------------------------------------------------------
+
+Then to run the scenario you must provide some web pages to download at the root of the node 0 
+at minimum you should provide a file named index.html under files-0
+then you can launch the simulation by calling:
+
+$BASEDCE/build/myscripts/httpd/bin/dce-httpd
+
+the downloaded files can be found under files-1/10.1.1.1 directory
+
+---
+
+ 
---
+++ myscripts/httpd/dce-httpd.cc
@@ -0,0 +1,119 @@
+// ===========================================================================
+//
+//   Topology
+//
+//         node 0                 node 1
+//   +----------------+    +----------------+
+//   |    ns-3 TCP    |    |    ns-3 TCP    |
+//   +----------------+    +----------------+
+//   |    10.1.1.1    |    |    10.1.1.2    |
+//   +----------------+    +----------------+
+//   | point-to-point |    | point-to-point |
+//   +----------------+    +----------------+
+//           |                     |
+//           +---------------------+
+//                100 Mbps, 1 ms
+//
+// This experience do http
+//
+// ===========================================================================
+
+#include "ns3/network-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/dce-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/trace-helper.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+using namespace ns3;
+void
+CreateFiles ()
+{
+  std::ofstream osf("files-0/index.html", std::fstream::trunc);
+  osf << "<HTML><HEAD><TITLE>Hello</TITLE></HEAD><BODY><H3>HELLO</H3>"
+      << "<P>is all right ?</BODY></HTML>" << std::endl;
+  osf.close ();
+}
+int
+main (int argc, char *argv[])
+{
+  bool useKernel = 0;
+
+  CommandLine cmd;
+  cmd.AddValue ("kernel", "Use kernel linux IP stack.", useKernel);
+  cmd.Parse (argc, argv);
+
+  mkdir ("files-0",0744);
+//  CreateFiles ();
+
+  NodeContainer nodes;
+  nodes.Create (2);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("1ms"));
+
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
+
+  DceManagerHelper dceManager;
+//  dceManager.SetTaskManagerAttribute( "FiberManagerType", StringValue ( "UcontextFiberManager" ) );
+
+  if (!useKernel)
+    {
+      InternetStackHelper stack;
+      stack.Install (nodes);
+    }
+  else
+    {
+      dceManager.SetNetworkStack ("ns3::LinuxSocketFdFactory", "Library", StringValue ("liblinux.so"));
+      LinuxStackHelper stack;
+      stack.Install (nodes);
+    }
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.252");
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  // setup ip routes
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  dceManager.Install (nodes);
+
+
+  DceApplicationHelper dce;
+  ApplicationContainer server, client;
+
+  dce.SetStackSize (1 << 20);
+
+  // Launch the server HTTP
+  dce.SetBinary ("thttpd");
+  dce.ResetArguments ();
+  dce.ResetEnvironment ();
+  dce.SetUid (1);
+  dce.SetEuid (1);
+  server = dce.Install (nodes.Get (0));
+  server.Start (Seconds (1));
+
+  dce.SetBinary ("wget");
+  dce.ResetArguments ();
+  dce.ResetEnvironment ();
+  dce.AddArgument("-r");
+  dce.AddArgument("http://10.1.1.1/index.html");
+
+  server = dce.Install (nodes.Get (1));
+  server.Start (Seconds (2));
+
+  pointToPoint.EnablePcapAll ("thttpd", false);
+
+  Simulator::Stop (Seconds (60.0));
+  Simulator::Run ();
+
+  Simulator::Destroy ();
+
+  return 0;
+}
+
