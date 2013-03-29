05e1ab8479da9a72ec5ab90a3d422eb8dda8e31b
Hajime Tazaki <tazaki@nict.go.jp>
2013-03-29 17:07:42 +0900

    update lcov parser

--- utils/lcov.sh
+++ utils/lcov.sh
@@ -18,14 +18,14 @@ for node in ${DIRS}
 do
 
 #gcov *.gcda
-find $1 -name "*.gcno" | cpio -pud ${node}/${APP_SRC_DIR} > /dev/null
-../ns-3-dev/utils/lcov/lcov -q -c -d ${node}${APP_SRC_DIR} -b ${APP_SRC_DIR} -o dce-run-${node}.info 
+find $1 -name "*.gcno" | grep -v files- | cpio -pud ${node}/ > /dev/null
+../ns-3-dev-dce/utils/lcov/lcov -q -c -d ${node}${APP_SRC_DIR} -b ${APP_SRC_DIR} -o dce-run-${node}.info 
 CMD_OPT="$CMD_OPT"" -a dce-run-${node}.info"
 
 done
 
-../ns-3-dev/utils/lcov/lcov -q ${CMD_OPT} -o dce-run.info
-../ns-3-dev/utils/lcov/genhtml -q --legend -o ${HTML_DIR} dce-run.info > /dev/null
+../ns-3-dev-dce/utils/lcov/lcov -q ${CMD_OPT} -o dce-run.info
+#../ns-3-dev-dce/utils/lcov/genhtml -q --legend -o ${HTML_DIR} dce-run.info > /dev/null
 
 rm -f dce-run-files-*.info
-rm -f dce-run.info
+#rm -f dce-run.info
