efb5889e0863d40ad50258290d506c5414358fc9
Hajime Tazaki <tazaki@nict.go.jp>
2013-04-09 18:22:55 +0900

    add dce__secure_getenv for newer oses

--- model/dce-env.cc
+++ model/dce-env.cc
@@ -21,6 +21,12 @@ char * dce_getenv (const char *name)
     }
   return seek_env (name, *penvp);
 }
+char * dce__secure_getenv (const char *name)
+{
+  // XXX
+  return dce_getenv (name);
+}
+
 int dce_putenv (char *string)
 {
   NS_LOG_FUNCTION (Current () << UtilsGetNodeId () << string);
