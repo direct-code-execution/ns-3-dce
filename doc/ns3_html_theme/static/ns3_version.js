3450e41ea1383aca7b4a7fc4c0feb6532b9d2f59
Hajime Tazaki <tazaki@sfc.wide.ad.jp>
2013-09-16 09:38:11 +0900

    add 1.1 version info

--- doc/ns3_html_theme/static/ns3_version.js
+++ doc/ns3_html_theme/static/ns3_version.js
@@ -1,7 +1,7 @@
 // ns3_version.js:  automatically generated
 //  public urls
 var ns3_host = "/";
-var ns3_version = "Release <a href=\"http://www.nsnam.org/overview/projects/direct-code-execution/dce-1.0\">dce-1.0</a>";
-var ns3_release = "docs/dce/release/1.0/";
+var ns3_version = "Release <a href=\"http://www.nsnam.org/overview/projects/direct-code-execution/dce-1.1\">dce-1.1</a>";
+var ns3_release = "docs/dce/release/1.1/";
 var ns3_local = "";
 var ns3_doxy  = "doxygen/";
