0a325ffff5b3dcf74f19754d27ba6d303725e424
Hajime Tazaki <tazaki@sfc.wide.ad.jp>
2013-12-16 21:46:29 +0900

    DCE 1.2 release

--- doc/ns3_html_theme/static/ns3_version.js
+++ doc/ns3_html_theme/static/ns3_version.js
@@ -1,7 +1,7 @@
 // ns3_version.js:  automatically generated
 //  public urls
 var ns3_host = "/";
-var ns3_version = "Release <a href=\"http://www.nsnam.org/overview/projects/direct-code-execution/dce-1.1\">dce-1.1</a>";
-var ns3_release = "docs/dce/release/1.1/";
+var ns3_version = "Release <a href=\"http://www.nsnam.org/overview/projects/direct-code-execution/dce-1.2\">dce-1.2</a>";
+var ns3_release = "docs/dce/release/1.2/";
 var ns3_local = "";
 var ns3_doxy  = "doxygen/";
