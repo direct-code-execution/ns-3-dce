21d9635d0a87d88f5e326220c46e751ffb3b29cd
Matthieu Coudron <mattator@gmail.com>
2016-01-06 14:00:21 +0100

    fix mkstemp behavior
    mkstemp should modify the suffix part "XXXXXX" of its argument so that
    the caller gets the correct filename

--- test/test-stdlib.cc
+++ test/test-stdlib.cc
@@ -17,8 +17,30 @@ static void test_strtod (void)
   TEST_ASSERT_EQUAL (retval, 2000);
 }
 
+static void test_mkstemp(void)
+{
+    /* dce_mkstemp */
+    int ret = 0;
+    std::string suffix = "XXXXXX";
+    std::string prefix = "test";
+    std::string oldFilename = prefix + suffix;
+    const std::size_t filenamelen = 30;
+    char filename[filenamelen];
+    strncpy (filename, oldFilename.c_str(), filenamelen - 1);
+    filename[oldFilename.size() - 1] = '\0';
+    ret = mkstemp (filename);
+    std::string newFilename (filename);
+    std::clog << "filename " << filename << std::endl;
+
+    /* test that the suffix part was actually changed and the prefix untouched */
+    TEST_ASSERT_EQUAL (newFilename.substr (0,4), prefix);
+    TEST_ASSERT_UNEQUAL (newFilename.substr (prefix.size()), suffix);
+
+}
+
 int main (int argc, char *argv[])
 {
   test_strtod ();
+  test_mkstemp ();
   return 0;
 }
