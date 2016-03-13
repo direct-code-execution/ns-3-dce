61d537ea62036bbfb7e46eb00c62d4d0e0e40fb8
Hajime Tazaki <tazaki@sfc.wide.ad.jp>
2016-03-13 22:08:41 +0900

    dce-1.8 release

--- doc/ns3_html_theme/static/ns3_version.js
+++ doc/ns3_html_theme/static/ns3_version.js
@@ -1,7 +1,7 @@
 // ns3_version.js:  automatically generated
 //  public urls
 var ns3_host = "/";
-var ns3_version = "Release <a href=\"http://www.nsnam.org/overview/projects/direct-code-execution/dce-1.7\">dce-1.7</a>";
-var ns3_release = "docs/dce/release/1.7/";
+var ns3_version = "Release <a href=\"http://www.nsnam.org/overview/projects/direct-code-execution/dce-1.8\">dce-1.8</a>";
+var ns3_release = "docs/dce/release/1.8/";
 var ns3_local = "";
 var ns3_doxy  = "doxygen/";
