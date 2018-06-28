962fdec16c23f1f33e01f3ca7a0157ec9750a31f
sourabhjains <jainsourabh679@gmail.com>
2018-06-28 13:19:41 +0530

    An example to test LKL with basic client server application

---
+++ example/basic-client-server.cc
@@ -0,0 +1,121 @@
+#include "ns3/network-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/dce-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/constant-position-mobility-model.h"
+#include "ccnx/misc-tools.h"
+
+using namespace ns3;
+NS_LOG_COMPONENT_DEFINE ("BasicClientServer");
+// ===========================================================================
+//
+//         node 0                 node 1
+//   +----------------+    +----------------+
+//   |                |    |                |
+//   +----------------+    +----------------+
+//   |    10.1.1.1    |    |    10.1.1.2    |
+//   +----------------+    +----------------+
+//   | point-to-point |    | point-to-point |
+//   +----------------+    +----------------+
+//           |                     |
+//           +---------------------+
+//                5 Mbps, 2 ms
+//
+// ===========================================================================
+int main (int argc, char *argv[])
+{
+  std::string stack = "ns3";
+  CommandLine cmd;
+  cmd.AddValue ("stack", "Name of IP stack: ns3/linux/freebsd.", stack);
+  cmd.Parse (argc, argv);
+
+  NodeContainer nodes;
+  nodes.Create (2);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
+
+  DceManagerHelper dceManager;
+  dceManager.SetTaskManagerAttribute ("FiberManagerType", StringValue ("UcontextFiberManager"));
+
+  if (stack == "ns3")
+    {
+      InternetStackHelper stack;
+      stack.Install (nodes);
+      dceManager.Install (nodes);
+    }
+  else if (stack == "linux")
+    {
+#ifdef KERNEL_STACK
+      dceManager.SetNetworkStack ("ns3::LinuxSocketFdFactory", "Library", StringValue ("liblkl.so"));
+      dceManager.Install (nodes);
+      LinuxStackHelper stack;
+      stack.Install (nodes);
+#else
+      NS_LOG_ERROR ("Linux kernel stack for DCE is not available. build with dce-linux module.");
+      // silently exit
+      return 0;
+#endif
+    }
+  else if
+    {
+      std::cout << "This example only support ns-3 and linux stack" << std::endl;
+      // silently exit
+      return 0;
+    }
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.252");
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  // setup ip routes
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+#ifdef KERNEL_STACK
+  if (stack == "linux")
+    {
+      LinuxStackHelper::PopulateRoutingTables ();
+    }
+#endif
+
+
+  DceApplicationHelper dce;
+  ApplicationContainer apps;
+
+  dce.SetStackSize (1 << 20);
+
+  // Launch iperf client on node 0
+  dce.SetBinary ("client");
+  dce.ResetArguments ();
+  dce.ResetEnvironment ();
+
+  apps = dce.Install (nodes.Get (0));
+  apps.Start (Seconds (0.7));
+  apps.Stop (Seconds (20));
+
+  // Launch iperf server on node 1
+  dce.SetBinary ("server");
+  dce.ResetArguments ();
+  dce.ResetEnvironment ();
+
+  apps = dce.Install (nodes.Get (1));
+
+  pointToPoint.EnablePcapAll ("basic-client-server-" + stack, false);
+
+  apps.Start (Seconds (0.6));
+
+  setPos (nodes.Get (0), 1, 10, 0);
+  setPos (nodes.Get (1), 50,10, 0);
+
+  Simulator::Stop (Seconds (40.0));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
